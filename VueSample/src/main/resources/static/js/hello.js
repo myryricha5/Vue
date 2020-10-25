a7a98e51deae24444d98793aec1420ef3a96187b
myryricha5 <risa_yasui@mail.benesse.co.jp>
2020-10-25 23:20:37 +0900

    firstPush

---
+++ VueSample/src/main/resources/static/js/hello.js
@@ -0,0 +1,59 @@
+var app = new Vue({
+
+	el: '#app',
+	data: {
+		message:'こんにちは、Vue.js！'
+	}
+});
+
+var app2 = new Vue({
+
+	el: '#app2',
+	data: {
+		message:'<h1>こんにちは</h1>' + '<span><a href="https://jp.vuejs.org/">Vue.js</a></span>'
+	}
+});
+
+var app3= new Vue({
+
+	el: '#app3',
+	data: {
+		url:'http://www.wings.msn.to/'
+	}
+});
+
+var app4= new Vue({
+
+	el: '#app4',
+	data: {
+		email:'Yamada@example.com'
+	}
+});
+
+var app5= new Vue({
+
+	el: '#app5',
+	data: {
+		email:'Yamada@example.com'
+	},
+	//emailプロパティの値を加工するlocalEmailメソッドを定義
+	methods: {
+		localEmail: function(){
+			return this.email.split('@')[0].toLowerCase();
+		}
+	}
+});
+
+var app6= new Vue({
+
+	el: '#app6',
+	data: {
+		email:'Yamada@example.com'
+	},
+	//emailプロパティの値を加工するlocalEmailメソッドを定義
+	methods: {
+		localEmail: function(){
+			return this.email.split('@')[0].toLowerCase();
+		}
+	}
+});
